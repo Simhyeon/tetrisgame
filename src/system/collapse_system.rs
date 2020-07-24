17f520e76dcd8a2f24f77c4c08d50066da69a74b
육심현 <simoncreek@tutanota.com>
2020-07-24 22:39:35 +0900

    Finished collapse system

--- src/system/collapse_system.rs
+++ src/system/collapse_system.rs
@@ -4,11 +4,12 @@ use amethyst::{
     ecs::prelude::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage, WriteExpect, LazyUpdate, Write, ReadExpect, Entity, Entities},
     shrev::{ReaderId, EventChannel},
 };
-use priority_queue::PriorityQueue;
+use std::collections::HashMap;
 
 use crate::component::stt_block::StaticBlock;
 use crate::system::stack_system::StackEvent;
 
+use std::cmp::Ordering;
 
 #[derive(SystemDesc)]
 pub struct CollapseSystem{
@@ -23,6 +24,20 @@ impl CollapseSystem {
     }
 }
 
+struct Container {
+    height: f32,
+    id: Entity,
+}
+
+impl Container {
+    pub fn new(height: f32, id: Entity) -> Self {
+        Self { 
+            height,
+            id,
+        }
+    }
+}
+
 impl<'s> System<'s> for CollapseSystem {
     type SystemData = (
         Entities<'s>,
@@ -33,7 +48,43 @@ impl<'s> System<'s> for CollapseSystem {
 
     fn run(&mut self, (entities, locals, stt_blocks, event_channel) : Self::SystemData) {
         for event in event_channel.read(&mut self.reader_id) {
-            println!("Received an event: {:?}", event);
+            if let StackEvent::Stacked = event {
+                // Stack Event called
+
+                let mut vector: Vec<Container> = Vec::new();
+                for (entity ,local, _) in (&entities, &locals, &stt_blocks).join() {
+                    vector.push(Container::new(local.translation().y, entity));
+                }
+
+                // Debug Code
+                //println!("Block size is : {}", vector.len());
+
+                // Sort vector array
+                vector.sort_unstable_by(|a,b| a.height.partial_cmp(&b.height).unwrap());
+
+                // Check Count
+                let mut prior: f32 = -1.0; // Inital value is -1
+                for counter in 0..vector.len() {
+                    if prior.partial_cmp(&vector[counter].height).unwrap() != Ordering::Equal {
+                        // Update index
+                        prior = vector[counter].height;
+                        println!("Found new transform : {}", prior);
+
+                        // Find all occurence
+                        let new_vec: Vec<&Container> = vector.iter().filter(|&x| x.height == prior).collect();
+
+                        // If line is full delte all entities
+                        if new_vec.len() == 10 { // Hard coded for now TODO SHould be soft coded
+                            println!("Found full line");
+                            for item in new_vec {
+                                entities.delete(item.id).expect("ERR");
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
--- src/system/stack_system.rs
+++ src/system/stack_system.rs
@@ -1,4 +1,5 @@
 use amethyst::{
+    core::math::Translation3,
     core::transform::Transform,
     derive::SystemDesc,
     ecs::prelude::{Join, System, SystemData, WriteStorage, ReadStorage, WriteExpect, Write},
@@ -18,50 +19,57 @@ pub enum StackEvent {
 pub struct StackSystem;
 
 impl<'s> System<'s> for StackSystem {
-  type SystemData = (
-      WriteExpect<'s, DynBlockHandler>,
-      ReadStorage<'s, DynamicBlock>,
-      WriteStorage<'s, StaticBlock>,
-      ReadStorage<'s, Transform>,
-      Write<'s, EventChannel<StackEvent>>,
-  );
+    type SystemData = (
+        WriteExpect<'s, DynBlockHandler>,
+        ReadStorage<'s, DynamicBlock>,
+        WriteStorage<'s, StaticBlock>,
+        ReadStorage<'s, Transform>,
+        Write<'s, EventChannel<StackEvent>>,
+    );
 
-  fn run(&mut self, (mut handler, dyn_blocks, mut stt_blocks, locals, mut event_channel): Self::SystemData) {
-      if handler.blocks.len() == 0 {
-        return;
-      }
-      for (_, local, ()) in (&dyn_blocks, &locals, !&stt_blocks).join() {
-          // get 이 consume 하는 지를 확인해 보자. 
-          if local.translation().y == 45.0 {
-              println!("{}", local.translation().y);
-              //stack = true;
-              for entity in handler.blocks.clone() {
-                  stt_blocks.insert(entity, StaticBlock).expect("ERR");
-              }
-              handler.blocks.clear();
-              event_channel.single_write(StackEvent::Stacked);
-              return;
-          }
-          // Check all locals without dynamicBlocks
-      }
-
-      let mut do_stack = false;
-      for (local, _) in (&locals, &stt_blocks).join() {
-        for entity in handler.blocks.clone() {
-            if locals.get(entity).unwrap().translation().y == local.translation().y + 45.0 {
-                do_stack = true;
-                println!("Stack upon other blocks");
+    fn run(&mut self, (mut handler, dyn_blocks, mut stt_blocks, locals, mut event_channel): Self::SystemData) {
+        if handler.blocks.len() == 0 {
+            return;
+        }
+        for (_, local, ()) in (&dyn_blocks, &locals, !&stt_blocks).join() {
+            // get 이 consume 하는 지를 확인해 보자. 
+            if local.translation().y == 45.0 {
+                println!("{}", local.translation().y);
+                //stack = true;
+                for entity in handler.blocks.clone() {
+                    stt_blocks.insert(entity, StaticBlock).expect("ERR");
+                }
+                handler.blocks.clear();
                 event_channel.single_write(StackEvent::Stacked);
-                break;
+                return;
+            }
+            // Check all locals without dynamicBlocks
+        }
+
+        let mut do_stack = false;
+        for (local, _) in (&locals, &stt_blocks).join() {
+            for entity in handler.blocks.clone() {
+                // Tempoary Values
+                let mut new_target = local.clone();
+                let target_pos = new_target.append_translation_xyz(0.0,45.0,0.0).translation();
+                let block_pos = locals.get(entity).unwrap().translation();
+
+                // 솔직히 왜 아직도 y 값이 같을 때 인지 잘 모르겠다... 분명 45 만큼 차이가 있는 게
+                // 정상 아닌가?
+                if block_pos.x == target_pos.x && block_pos.y == target_pos.y {
+                    do_stack = true;
+                    println!("Stack upon other blocks");
+                    event_channel.single_write(StackEvent::Stacked);
+                    break;
+                }
             }
         }
-      }
 
-      if do_stack {
-          for entity in handler.blocks.clone() {
-              stt_blocks.insert(entity, StaticBlock).expect("ERR");
-          }
-          handler.blocks.clear();
-      }
-  }
+        if do_stack {
+            for entity in handler.blocks.clone() {
+                stt_blocks.insert(entity, StaticBlock).expect("ERR");
+            }
+            handler.blocks.clear();
+        }
+    }
 }
